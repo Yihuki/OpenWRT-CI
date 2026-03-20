b7270062bebb9f9f8368ef3cf71471a8e9f85d84
Yihuki <wangbovbvb@gmail.com>
2026-03-20 08:54:47 +0800

    Add luci-app-tinyfilemanager

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -74,6 +74,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "luci-app-tinyfilemanager" "muink/luci-app-tinyfilemanager" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
